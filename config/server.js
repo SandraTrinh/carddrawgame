ac026684c6777071e78d11a1a2fc509e35ecac00
SandraTrinh <69127944+SandraTrinh@users.noreply.github.com>
2021-06-09 16:54:34 +1000

    reset code to default

--- config/server.js
+++ config/server.js
@@ -5,10 +5,5 @@ module.exports = ({ env }) => ({
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'acd0e1abc7b133f63357c91d9a33b568'),
     },
-  },
-  url: 'https://carddrawgame.herokuapp.com',
-  admin: {
-    url: '/',
-    serverAdminPanel: false
   }
 });
