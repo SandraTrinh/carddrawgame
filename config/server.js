377c6d2be76fcd3008877489792dcc26af746bd3
SandraTrinh <69127944+SandraTrinh@users.noreply.github.com>
2021-06-09 16:19:02 +1000

    try admin code in server.js

--- config/server.js
+++ config/server.js
@@ -6,4 +6,9 @@ module.exports = ({ env }) => ({
       secret: env('ADMIN_JWT_SECRET', 'acd0e1abc7b133f63357c91d9a33b568'),
     },
   },
+  url: 'https://carddrawgame.herokuapp.com',
+  admin: {
+    url: '/',
+    serverAdminPanel: false
+  }
 });
